0476cc5f5b6f5fea21d91e2302fb0e35bd05f5c2
Harika Nittala <lnittala@amd.com>
2024-12-03 00:02:58 +0000

    snp.sh: Create and launch multiple SNP enabled guests
    Users can launch multiple SNP enabled guests at the same time via the export of the GUEST_NAME and HOST_SSH_PORT env. variables
    
    For the launch of multiple SNP guest at the same time, modified snp.sh to create a separate guest user directory and point the launch working directory to point the current specific guest user directory.
    
    Signed-off-by: Harika Nittala <lnittala@amd.com>

--- tools/snp.sh
+++ tools/snp.sh
@@ -68,7 +68,8 @@ trap cleanup EXIT
 # Working directory setup
 WORKING_DIR="${WORKING_DIR:-$HOME/snp}"
 SETUP_WORKING_DIR="${SETUP_WORKING_DIR:-${WORKING_DIR}/setup}"
-LAUNCH_WORKING_DIR="${LAUNCH_WORKING_DIR:-${WORKING_DIR}/launch}"
+GUEST_NAME="${GUEST_NAME:-snp-guest}"
+LAUNCH_WORKING_DIR="${LAUNCH_WORKING_DIR:-${WORKING_DIR}/launch/${GUEST_NAME}}"
 ATTESTATION_WORKING_DIR="${ATTESTATION_WORKING_DIR:-${WORKING_DIR}/attest}"
 
 # Export environment variables
@@ -76,7 +77,6 @@ COMMAND="help"
 UPM=true
 SKIP_IMAGE_CREATE=false
 HOST_SSH_PORT="${HOST_SSH_PORT:-10022}"
-GUEST_NAME="${GUEST_NAME:-snp-guest}"
 GUEST_SIZE_GB="${GUEST_SIZE_GB:-20}"
 GUEST_MEM_SIZE_MB="${GUEST_MEM_SIZE_MB:-2048}"
 GUEST_SMP="${GUEST_SMP:-4}"
@@ -503,8 +503,8 @@ generate_guest_ssh_keypair() {
 }
 
 cloud_init_create_data() {
-  if [[ -f "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-metadata.yaml" && \
-    -f "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-user-data.yaml"  && \
+  if [[ -f "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/metadata.yaml" && \
+    -f "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/user-data.yaml"  && \
     -f "${IMAGE}" ]]; then
     echo -e "cloud-init data already generated"
     return 0
@@ -513,13 +513,13 @@ cloud_init_create_data() {
   local pub_key=$(cat "${GUEST_SSH_KEY_PATH}.pub")
 
 # Seed image metadata
-cat > "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-metadata.yaml" <<EOF
+cat > "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/metadata.yaml" <<EOF
 instance-id: "${GUEST_NAME}"
 local-hostname: "${GUEST_NAME}"
 EOF
 
 # Seed image user data
-cat > "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-user-data.yaml" <<EOF
+cat > "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/user-data.yaml" <<EOF
 #cloud-config
 chpasswd:
   expire: false
@@ -537,8 +537,8 @@ EOF
 
   # Create the seed image with metadata and user data
   cloud-localds "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-seed.img" \
-    "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-user-data.yaml" \
-    "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-metadata.yaml"
+    "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/user-data.yaml" \
+    "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data/metadata.yaml"
 
   # Download ubuntu 20.04 and change name
   if [ ! -f "${BASE_CLOUD_IMAGE}" ]; then
@@ -717,6 +717,9 @@ copy_launch_binaries() {
 
   # Create directory
   mkdir -p "${LAUNCH_WORKING_DIR}"
+
+  # Create a separate guest data directory
+  mkdir -p "${LAUNCH_WORKING_DIR}/${GUEST_NAME}-data"
 
   # Copy the setup generated bins to the guest launch directory
   # initrd is copied after the first guest boot and is scp-ed off
